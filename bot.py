4cde3cc600ade62958f820d1ef13dbe42f0c965f
rorolb <nbveh2113@gmail.com>
2025-11-22 21:37:23 +0300

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+#бан
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
@@ -22,5 +23,62 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+#мут
+@bot.message_handler(commands=['mute'])
+def mute_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            duration = 60 # Значение по умолчанию - 1 минута
+            args = message.text.split()[1:]
+            if args:
+                try:
+                    duration = int(args[0])
+                except ValueError:
+                    bot.reply_to(message, "Неправильный формат времени.")
+                    return
+                if duration < 1:
+                    bot.reply_to(message, "Время должно быть положительным числом.")
+                    return
+                if duration > 1440:
+                    bot.reply_to(message, "Максимальное время - 1 день.")
+                    return
+            bot.restrict_chat_member(chat_id, user_id, until_date=time.time()+duration*60)
+            bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} замучен на {duration} минут.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите замутить.")
+
+#анмут
+@bot.message_handler(commands=['unmute'])
+def unmute_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        bot.restrict_chat_member(chat_id, user_id, can_send_messages=True, can_send_media_messages=True, can_send_other_messages=True, can_add_web_page_previews=True)
+        bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} размучен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите размутить.")
+
+#кик
+@bot.message_handler(commands=['kick'])
+def kick_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно кикнуть администратора.")
+        else:
+            bot.kick_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} был кикнут.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите кикнуть.")
 
 bot.infinity_polling(none_stop=True)
